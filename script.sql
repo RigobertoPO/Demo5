0cc1bef3806ef653d9ca2a4784860e0b5b04e707
RigobertoPO <rperez@cobach.edu.mx>
2023-04-18 17:43:05 -0600

    modificar script

--- script.sql
+++ script.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS  Almacen;
 use almacen;
 create table Productos(
-    Id int PRIMARY key auto INCREMENT,
+    Id int PRIMARY key AUTO_INCREMENT,
     Nombre varchar(120),
     Precio float,
     Existencias Int
